dc9126dfac8c6cd3462e8e7c701f16002ebe3668
iamageo <geovani.af4@gmail.com>
2022-07-30 19:10:06 -0300

    testing library

--- app/src/main/java/com/iamageo/bank_card_view/MainActivity.kt
+++ app/src/main/java/com/iamageo/bank_card_view/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.iamageo.bank_card_view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iamageo.bank_card_view.databinding.ActivityMainBinding
+import com.iamageo.bank_card_view_android.FlipBankCardViewAnimation
+import com.iamageo.bank_card_view_android.startAnimation
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val animation: FlipBankCardViewAnimation? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.myBankCardView.bankCardName = "Jhon Doe"
+        binding.myBankCardView.bankCardNumber = "0001 0002 0003 0004"
+        binding.myBankCardView.bankCardValidate = "02/27"
+
+        binding.myBankCardView.setOnClickListener {
+            startAnimation(animation, binding.myBankCardView, binding.myBankCardView, 180)
+        }
     }
 }
