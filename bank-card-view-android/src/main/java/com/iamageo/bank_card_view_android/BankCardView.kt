4caed30d73b97e1345af2eba472c3327ec241dac
iamageo <geovani.af4@gmail.com>
2022-09-04 20:54:39 -0300

    update library to use view binding

--- bank-card-view-android/src/main/java/com/iamageo/bank_card_view_android/BankCardView.kt
+++ bank-card-view-android/src/main/java/com/iamageo/bank_card_view_android/BankCardView.kt
@@ -1,19 +1,21 @@
 package com.iamageo.bank_card_view_android
 
 import android.content.Context
-import android.text.Editable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.bank_card_view.view.*
+import com.iamageo.bank_card_view_android.databinding.BankCardViewBinding
 
 class BankCardView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private var binding: BankCardViewBinding =
+        BankCardViewBinding.inflate(LayoutInflater.from(context))
 
     enum class CardLogo {
         VISA,
@@ -32,49 +34,69 @@ class BankCardView @JvmOverloads constructor(
     var bankCardName: String? = ""
         set(value) {
             field = value
-            bank_card_view_name.text = field
+            binding.bankCardViewName.text = field
         }
-
-    /*
-    var bankCardNumber: Editable? = null
-        set(value) {
-            field = value
-            bank_card_view_number.text = field
-        }
-     */
 
     var bankCardValidate: String? = ""
         set(value) {
             field = value
-            bank_card_view_validate_date.text = field
+            binding.bankCardViewValidateDate.text = field
         }
 
-    var bankCardCVV: String ? = ""
+    var bankCardCVV: String? = ""
         set(value) {
             field = value
-            bank_card_view_cvv.text = field
+            binding.bankCardViewCvv.text = field
         }
 
     var bankCardLogo: CardLogo? = null
         set(value) {
             field = value
-            when(value) {
-                CardLogo.CIELO -> { bank_card_view_logo.setImageResource(R.drawable.ic_bank_card_view_cielo) }
-                CardLogo.AMERICAN_EXPRESS -> { bank_card_view_logo.setImageResource(R.drawable.ic_bank_card_view_american_express) }
-                CardLogo.VISA -> { bank_card_view_logo.setImageResource(R.drawable.ic_bank_card_view_visa) }
-                CardLogo.MASTERCARD -> {bank_card_view_logo.setImageResource(R.drawable.ic_bank_card_view_mastercard) }
-                else -> {  }
+            when (value) {
+                CardLogo.CIELO -> {
+                    binding.bankCardViewLogo.setImageResource(R.drawable.ic_bank_card_view_cielo)
+                }
+                CardLogo.AMERICAN_EXPRESS -> {
+                    binding.bankCardViewLogo.setImageResource(R.drawable.ic_bank_card_view_american_express)
+                }
+                CardLogo.VISA -> {
+                    binding.bankCardViewLogo.setImageResource(R.drawable.ic_bank_card_view_visa)
+                }
+                CardLogo.MASTERCARD -> {
+                    binding.bankCardViewLogo.setImageResource(R.drawable.ic_bank_card_view_mastercard)
+                }
+                else -> {}
             }
         }
 
     var bankCardBackground: CardBackground? = null
-        set(value){
+        set(value) {
             field = value
-            when(value) {
-                CardBackground.AMY_CHRISP -> { bank_card_view.background = ContextCompat.getDrawable(context, (R.drawable.bank_card_view_gradient_amy_chrisp)) }
-                CardBackground.RIPE_MALINKA -> {  bank_card_view.background = ContextCompat.getDrawable(context, (R.drawable.bank_card_view_gradient_ripe_malinka))  }
-                CardBackground.TRUE_SUNSET -> {  bank_card_view.background = ContextCompat.getDrawable(context, (R.drawable.bank_card_view_gradient_true_sunset))  }
-                CardBackground.PURPLE_DIVISION -> {  bank_card_view.background = ContextCompat.getDrawable(context, (R.drawable.bank_card_view_gradient_purple_division))  }
+            when (value) {
+                CardBackground.AMY_CHRISP -> {
+                    binding.bankCardView.background = ContextCompat.getDrawable(
+                        context,
+                        (R.drawable.bank_card_view_gradient_amy_chrisp)
+                    )
+                }
+                CardBackground.RIPE_MALINKA -> {
+                    binding.bankCardView.background = ContextCompat.getDrawable(
+                        context,
+                        (R.drawable.bank_card_view_gradient_ripe_malinka)
+                    )
+                }
+                CardBackground.TRUE_SUNSET -> {
+                    binding.bankCardView.background = ContextCompat.getDrawable(
+                        context,
+                        (R.drawable.bank_card_view_gradient_true_sunset)
+                    )
+                }
+                CardBackground.PURPLE_DIVISION -> {
+                    binding.bankCardView.background = ContextCompat.getDrawable(
+                        context,
+                        (R.drawable.bank_card_view_gradient_purple_division)
+                    )
+                }
                 else -> {}
             }
         }
@@ -99,17 +121,18 @@ class BankCardView @JvmOverloads constructor(
     }
 
     private fun showCardContent(isBackCard: Boolean) {
-        bankCardViewBack.visibility = if (isBackCard) View.VISIBLE else View.GONE
-        bankCardViewFront.visibility = if (isBackCard) View.GONE else View.VISIBLE
+        binding.bankCardViewBack.visibility = if (isBackCard) View.VISIBLE else View.GONE
+        binding.bankCardViewFront.visibility = if (isBackCard) View.GONE else View.VISIBLE
     }
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.bank_card_view, this, true)
+        addView(binding.root)
         extractAttrs(attrs, defStyleAttr)
     }
 
     private fun extractAttrs(attrs: AttributeSet?, defStyleAttr: Int = 0) {
-        val array = context.theme.obtainStyledAttributes(attrs, R.styleable.BankCardView, defStyleAttr, 0)
+        val array =
+            context.theme.obtainStyledAttributes(attrs, R.styleable.BankCardView, defStyleAttr, 0)
         isFrontCard = array.getBoolean(R.styleable.BankCardView_isFrontCard, true)
         bankCardName = array.getString(R.styleable.BankCardView_bankCardName)
         //bankCardNumber = array.getString(R.styleable.BankCardView_bankCardNumber)
